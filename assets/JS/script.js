0fff3e65c6c1711f271c10256dc86a04ea7d46c4
Matt Lahey <mdlahey@outlook.com>
2022-10-25 15:06:27 -0400

    added additionaly variables for API fetch and added timezone plugins to day.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,10 @@
-var currentDate = "(" + dayjs().format("MM/DD/YYYY") + ")";
+//API Variables
+var weatherApiRootURL = "https://api.openweathermap.org";
+var weatherApiKey = "114cbc8355fa237b4bef43c392e276d6";
+
+//Element Variables
 var cityForm = document.querySelector("#cityForm");
 var citySearchInput = document.querySelector("#citySearchInput");
-var searchHistoryArr = [];
 var searchHistory = document.querySelector("#searchHistory");
 var historyBtns = document.querySelector("#historyBtns");
 var historyCard = document.querySelector("#historyCard");
@@ -11,4 +14,14 @@ var currentWeatherCard = document.querySelector("#currentWeatherCard");
 var weatherStatus = document.querySelector("#weatherStatus");
 var fiveDayCard = document.querySelector("#fiveDayCard");
 var fiveDayBody = document.querySelector("#fiveDayBody");
+
+//Array to store search history
+var searchHistoryArr = [];
+
+// Add timezone plugins to day.js
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+//Variable to store the current date
+var currentDate = "(" + dayjs().format("MM/DD/YYYY") + ")";
 
